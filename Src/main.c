a7fbcf31bb6b9581d29d8f357b93063741c4f5dc
Sergey-PC\Sergey <asergeyv177@gmail.com>
2018-09-19 21:21:40 +0300

    Градусы изменения положения датчика

---
+++ Src/main.c
@@ -0,0 +1,632 @@
+
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+	
+#pragma push
+#pragma O0
+
+
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include <math.h>
+
+
+#define M_PI       3.16159265358979323846
+
+
+ADC_HandleTypeDef hadc1;
+ADC_HandleTypeDef hadc2;
+
+DAC_HandleTypeDef hdac;
+
+I2C_HandleTypeDef hi2c1;
+
+TIM_HandleTypeDef htim1;
+
+
+struct AccelerationXYZAxis
+{
+	float ax;
+	float ay;
+	float az;
+};
+
+struct AngleOfRotationXYZAxis
+{
+	float xAngle;
+	float yAngle;
+	float zAngle;
+};
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_ADC2_Init(void);
+static void MX_DAC_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_TIM1_Init(void);
+void ledBlinkWithFrequency(int frequency);
+
+void prepareI2C();
+int readI2C(int addr);
+
+float obtainAccelerationXAxis();
+float obtainAccelerationYAxis();
+float obtainAccelerationZAxis();
+float angleFromProjections(float secondaryCatheter, float primaryCatheter_1, float primaryCatheter_2);
+struct AngleOfRotationXYZAxis obtainCurrentAngleOfRotation();
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+                                
+
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  MX_ADC2_Init();
+  MX_DAC_Init();
+  MX_I2C1_Init();
+  MX_TIM1_Init();
+  /* USER CODE BEGIN 2 */
+	prepareI2C();
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+//	volatile int16_t valueGyro = 0;
+	struct AngleOfRotationXYZAxis axyzAngles = {0,0,0};
+  while (1)
+  {
+		axyzAngles = obtainCurrentAngleOfRotation();
+//		valueGyro = Gyro();
+//		ledBlinkWithFrequency(valueGyro);
+  }
+}
+
+struct AngleOfRotationXYZAxis obtainCurrentAngleOfRotation()
+{
+	struct AccelerationXYZAxis axyzAxis = {0,0,0};
+	axyzAxis.ax = obtainAccelerationXAxis();
+	axyzAxis.ay = obtainAccelerationYAxis();
+	axyzAxis.az = obtainAccelerationZAxis();
+	
+	struct AngleOfRotationXYZAxis xyzAngles = {0,0,0};
+	xyzAngles.xAngle = angleFromProjections(axyzAxis.ax, axyzAxis.ay, axyzAxis.az);
+	xyzAngles.yAngle = angleFromProjections(axyzAxis.ay, axyzAxis.az, axyzAxis.ax);
+	xyzAngles.zAngle = angleFromProjections(axyzAxis.az, axyzAxis.ax, axyzAxis.ay);
+	
+	return xyzAngles;
+}
+
+float angleFromProjections(float secondaryCatheter, float primaryCatheter_1, float primaryCatheter_2)
+{
+	float hypotenuse = sqrt(pow(primaryCatheter_1,2) + pow(primaryCatheter_2,2));
+	return (atan2(secondaryCatheter, hypotenuse) * 180 / M_PI);
+}
+
+float obtainAccelerationXAxis()
+{
+	int16_t ax =(readI2C(0x3B)<<8);
+	ax|=readI2C(0x3C);
+	return (float)ax / 32767;
+}
+
+float obtainAccelerationYAxis()
+{
+	int16_t ay =(readI2C(0x3D)<<8);
+	ay|=readI2C(0x3E);
+	return (float)ay / 32767;
+}
+
+float obtainAccelerationZAxis()
+{
+	int16_t az =(readI2C(0x3F)<<8);
+	az|=readI2C(0x40);
+	return (float)az / 32767;
+}
+
+int readI2C(int addr)
+{
+	I2C1->CR1 = 0xC;
+	I2C1->OAR1 = (0x4000);
+	I2C1->CCR = 0x3C;
+	I2C1->TRISE = 0xD;
+	I2C1->CR1 = I2C_CR1_PE;
+	for(int i=0; i<0xFFF; i++){}		
+	I2C1->CR1 |= I2C_CR1_START;
+	while (!(I2C1->SR1 & I2C_SR1_SB)) {}
+	(void) I2C1->SR1;
+	I2C1->DR = 0xD0;
+	while (!(I2C1->SR1 & I2C_SR1_ADDR)){}
+	(void) I2C1->SR1;
+	(void) I2C1->SR2;
+	I2C1->DR = addr;		
+	while (!(I2C1->SR1 & I2C_SR1_BTF)){}
+		
+//---------------------------------//
+		
+	I2C1->CR1 |= I2C_CR1_START;
+	while (!(I2C1->SR1 & I2C_SR1_SB)){}
+	(void) I2C1->SR1;
+	I2C1->DR = 0xD1;
+	while (!(I2C1->SR1 & I2C_SR1_ADDR)){}
+	(void) I2C1->SR1;
+	(void) I2C1->SR2;
+	
+//---------------------------------//
+		
+	while (!(I2C1->SR1 & I2C_SR1_RXNE)){}
+	volatile int data_i2c = I2C1->DR;
+	I2C1->CR1 |= I2C_CR1_ACK;
+	(void) I2C1->SR1;
+	(void) I2C1->SR2;
+	I2C1->CR1 |= I2C_CR1_STOP;
+	(void) I2C1->SR1;
+	(void) I2C1->SR2;
+	data_i2c = I2C1->DR;		
+	I2C1->CR1 |= I2C_CR1_STOP;
+	while(I2C1->SR1 & I2C_SR1_STOPF){}
+	while((I2C1->SR2)&&(0x2)==0x2){}
+	I2C1->CR1 |= I2C_CR1_SWRST;
+	for(int i=0; i<0xFF; i++){}
+	return data_i2c;
+}
+
+void prepareI2C()
+{
+//---------------------------------//
+
+		I2C1->CR1 |= I2C_CR1_PE;
+		I2C1->CR1 |= I2C_CR1_START;
+		while (!(I2C1->SR1 & I2C_SR1_SB)){}
+		(void) I2C1->SR1;
+		I2C1->DR = 0xD0;
+		while (!(I2C1->SR1 & I2C_SR1_ADDR)){}
+		(void) I2C1->SR1;
+		(void) I2C1->SR2;
+		I2C1->DR = 0x6B;
+		while (!(I2C1->SR1 & I2C_SR1_BTF)){}
+		I2C1->DR = 0x00;
+		while (!(I2C1->SR1 & I2C_SR1_BTF)){}
+		I2C1->CR1 |= I2C_CR1_STOP;
+
+//---------------------------------//
+
+		I2C1->CR1 |= I2C_CR1_START;
+		while (!(I2C1->SR1 & I2C_SR1_SB)){}
+		(void) I2C1->SR1;
+		I2C1->DR = 0xD0;
+		while (!(I2C1->SR1 & I2C_SR1_ADDR)){}
+		(void) I2C1->SR1;
+		(void) I2C1->SR2;
+		I2C1->DR = 0x38;
+		while (!(I2C1->SR1 & I2C_SR1_BTF)){}
+		I2C1->DR = 0x01;
+		while (!(I2C1->SR1 & I2C_SR1_BTF)){}
+		I2C1->CR1 |= I2C_CR1_STOP;
+
+//---------------------------------//
+		
+		I2C1->CR1 |= I2C_CR1_START;
+		while (!(I2C1->SR1 & I2C_SR1_SB)){}
+		(void) I2C1->SR1;
+		I2C1->DR = 0xD0;
+		while (!(I2C1->SR1 & I2C_SR1_ADDR)){}
+		(void) I2C1->SR1;
+		(void) I2C1->SR2;
+		I2C1->DR = 0x1A;
+		while (!(I2C1->SR1 & I2C_SR1_BTF)){}
+		I2C1->DR = 0x04;
+		while (!(I2C1->SR1 & I2C_SR1_BTF)){}
+		I2C1->CR1 |= I2C_CR1_STOP;
+
+//---------------------------------//
+
+		I2C1->CR1 |= I2C_CR1_START;
+		while (!(I2C1->SR1 & I2C_SR1_SB)){}
+		(void) I2C1->SR1;
+		I2C1->DR = 0xD0;
+		while (!(I2C1->SR1 & I2C_SR1_ADDR)){}
+		(void) I2C1->SR1;
+		(void) I2C1->SR2;
+		I2C1->DR = 0x19;
+		while (!(I2C1->SR1 & I2C_SR1_BTF)){}
+		I2C1->DR = 0x00;
+		while (!(I2C1->SR1 & I2C_SR1_BTF)){}
+		I2C1->CR1 |= I2C_CR1_STOP;
+		for(int i=0; i<0xFF; i++){}
+}
+
+void ledBlinkWithFrequency(int frequency)
+{
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_0, GPIO_PIN_SET);
+	for(int i = 0; i < frequency / 2; ++i){}
+//	HAL_Delay(frequency / 2);
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_0, GPIO_PIN_RESET);
+	for(int i = 0; i < frequency / 2; ++i){}
+//	HAL_Delay(frequency / 2);
+}
+
+#pragma pop
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Configure the main internal regulator output voltage 
+    */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* ADC1 init function */
+static void MX_ADC1_Init(void)
+{
+
+  ADC_ChannelConfTypeDef sConfig;
+
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+    */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV6;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* ADC2 init function */
+static void MX_ADC2_Init(void)
+{
+
+  ADC_ChannelConfTypeDef sConfig;
+
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+    */
+  hadc2.Instance = ADC2;
+  hadc2.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV6;
+  hadc2.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc2.Init.ScanConvMode = DISABLE;
+  hadc2.Init.ContinuousConvMode = DISABLE;
+  hadc2.Init.DiscontinuousConvMode = DISABLE;
+  hadc2.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc2.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc2.Init.NbrOfConversion = 1;
+  hadc2.Init.DMAContinuousRequests = DISABLE;
+  hadc2.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* DAC init function */
+static void MX_DAC_Init(void)
+{
+
+  DAC_ChannelConfTypeDef sConfig;
+
+    /**DAC Initialization 
+    */
+  hdac.Instance = DAC;
+  if (HAL_DAC_Init(&hdac) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**DAC channel OUT1 config 
+    */
+  sConfig.DAC_Trigger = DAC_TRIGGER_NONE;
+  sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+  if (HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**DAC channel OUT2 config 
+    */
+  if (HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* I2C1 init function */
+static void MX_I2C1_Init(void)
+{
+
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* TIM1 init function */
+static void MX_TIM1_Init(void)
+{
+
+  TIM_ClockConfigTypeDef sClockSourceConfig;
+  TIM_MasterConfigTypeDef sMasterConfig;
+  TIM_OC_InitTypeDef sConfigOC;
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
+
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 0;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  if (HAL_TIM_OC_Init(&htim1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sConfigOC.OCMode = TIM_OCMODE_TIMING;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_OC_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_0, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PE0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  file: The file name as string.
+  * @param  line: The line in file as a number.
+  * @retval None
+  */
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
